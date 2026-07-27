b2663a5e394592f9927cf2c4d367c55a4d491e46
Elif Nur Beycan <elif.bycn35@gmail.com>
2026-07-27 09:00:59 +0300

    feat: add mandatory password change on first login

--- src/presentation/App.jsx
+++ src/presentation/App.jsx
@@ -1,4 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import { ChangePasswordPage } from "./pages/ChangePasswordPage";
 import { DashboardPage } from "./pages/DashboardPage";
 import { LoginPage } from "./pages/LoginPage";
 import { RoomsPage } from "./pages/RoomsPage";
@@ -8,6 +9,7 @@ export function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/change-password" element={<ChangePasswordPage />} />
       <Route path="/super-admin/login" element={<SuperAdminLoginPage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
       <Route path="/rooms" element={<RoomsPage />} />
